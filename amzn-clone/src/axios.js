0c47f5c3c674aa198bcc398d22f30023e1d68ccd
yanzayd <nelsonnyzbedel@gmail.com>
2022-08-29 18:20:28 +0200

    linkedin-clone

--- amzn-clone/src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: 'http://localhost:5001/e-commerce-76b44/us-central1/api' //THE API (cloud function) URL 
-    //'https://us-central1-e-commercer-76b44.cloudfunctions.net/api'
-});
-
-export default instance;
